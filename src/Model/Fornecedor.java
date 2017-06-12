85063721d38057c8113f622362d7c9a070aa9b1c
Jefferson Vantuir <jefferson.behling@gmail.com>
2017-06-12 15:43:42 -0300

    Criacao de Model de Fornecedor

---
+++ src/Model/Fornecedor.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Jefferson Vantuir
+ */
+public class Fornecedor {
+    String cnpj;
+    String nomeFornecedor;
+
+    public String getCNPJ() {
+        return cnpj;
+    }
+
+    public void setCNPJ(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getNomeFornecedor() {
+        return nomeFornecedor;
+    }
+
+    public void setNomeFornecedor(String nomeFornecedor) {
+        this.nomeFornecedor = nomeFornecedor;
+    }
+    
+    
+}
